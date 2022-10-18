a1bbc4c063d2a41aa18aaa24890a8ffb0b6c6521
Goran Kekez <gorankekez@gmail.com>
2022-10-18 12:51:41 +0200

    added recipes and favorites to local storage - maxi

--- recipe-app/main.js
+++ recipe-app/main.js
@@ -1,23 +1,18 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+
+// Recipes add to local storage
+function recipesAddToLocalStorage() {
+  let recipes = [];
+  localStorage.recipes = JSON.stringify(recipes);
+}
+
+// Favorites add to local storage
+function favoritesAddToLocalStorage() {
+  let favorites = [];
+  localStorage.favorites = JSON.stringify(favorites);
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Toucans team 2!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
